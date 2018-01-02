ab5a87ed0b278049feb2b0ab47dde00c548c91bb
Tomek Korbak <tomasz.korbak@gmail.com>
2018-01-03 00:56:32 +0100

    Fix fetch_data.sh script and update requirements

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Creating directories"
-mkdir -p data/
-mkdir -p data/pol/
-mkdir -p models/
-mkdir -p results/
-mkdir -p finals/
-mkdir tmp/
+mkdir -p data/pol/ models/ results/ finals/ treehopper/tmp/
 
 echo "Download training set"
 curl -O http://poleval.pl/task2/sentiment-treebank.tar.gz
@@ -20,14 +15,13 @@ mkdir -p data/pol/fasttext
 unzip wiki.pl.zip
 rm wiki.pl.zip
 
-echo "Download and build fastText from source"
+#echo "Download and build fastText from source"
 #git clone https://github.com/facebookresearch/fastText.git
 #make fastText
 
-echo "Install Python requirements"
+#echo "Install Python requirements"
 #pip install -r requirements.txt
 #pip install http://download.pytorch.org/whl/cu80/torch-0.1.12.post2-cp35-cp35m-linux_x86_64.whl
-
 
 echo "Download test set"
 curl -O http://mozart.ipipan.waw.pl/~axw/poleval/poleval_test.tar.gz
